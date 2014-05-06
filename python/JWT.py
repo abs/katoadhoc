98895548e6ca5dc9cdcfe5272f5241ad39a76d83
Jasmin Gacic <jasmin.gacic@gmail.com>
2014-05-06 14:00:55 +0200

    Python code

--- python/JWT.py
+++ python/JWT.py
@@ -1,14 +1,15 @@
 def service(request):
-	pkey = "EK1UcCeetSxCVAYEZw9zxr6iCHX0gm1JSSkamHVcxVQ"
-	skey = "pTif3EypiNOYIV8_QKQvm3KdJugGjSXVRkHdi0CX_1k"
+	pkey = "__pkey__"
+	skey = "__skey__"
 
-	user_id = "0"
-	user_name = "bars"
-	room_id = "0"
-	room_name = "bar"
-	
+	user_id = "__userid__"
+	user_name = "__username__"
+	room_id = "__roomid__"
+	room_name = "__roomname__"
+	duration = "__duration__"
+    
 	from datetime import datetime, timedelta
-	expDate = datetime.now() + timedelta(hours=1)
+	expDate = datetime.now() + timedelta(seconds=duration)
 	
 	payload = '{"exp":'+str(expDate.timestamp())+',"user_id":"'+user_id+'","user_name":"'+user_name+'","room_id":"'+room_id+'","room_name":"'+room_name+'"}';
 	
