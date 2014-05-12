78abdb79f55d3c0316337863f89c5f3f2d1bede9
Jasmin Gacic <jasmin.gacic@gmail.com>
2014-05-12 15:54:02 +0200

    node code with package

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,19 +1,46 @@
 var jwt = require('jwt-simple');
 var express = require('express');
-var app = express();
+var kato = require('kato-adhoc-express');
 var fs = require('fs');
+var app = express();
+
 var index = fs.readFileSync('index.html');
 
-var payload = {
-	exp : Math.round((new Date().getTime() / 1000)) + __duration__,
-	user_id : '__userid__',
-	user_name : '__username__',
-	room_id : '__roomid__',
-	room_name : '__roomname__'
+var katoSettings = {
+	    // Required
+    pKey: '__pkey__',
+
+    // Required
+    sKey: '__skey__',
+
+    // Required, expiration epoch in seconds
+    exp: __duration__,
+
+    // Required, unique user id
+    user_id : __userid__,
+
+    // Required, unique room id
+    user_name : '__username__',
+
+    // Required, unique room id
+    room_id : __roomid__,
+
+    // Required, room name                
+    room_name : '__roomname__',
+
+	//**********NOTE*********** 
+	//If these values are not used please remov
+    // Optional, user email address (visible in tooltip over user name in history) must be unique when used within the scope of a given public key
+    user_email: '',
+
+    // Optional, text message to be sent out by welcome robot when room is created
+    welcome_text: '',
+
+    // Optional, name of welcome robot, if not specified defaults to 'Welcome Robot'
+    welcome_robot_name: '', 
 };
-var pkey = "__pkey__";
-var skey = "__skey__";
-var token = 'https://kato.im/adhoc#/' + pkey + '/' + jwt.encode(payload, skey);
+
+var token = kato.generateUrl(katoSettings);
 
 app.get('/', function(req, res){
    res.writeHead(200, {'Content-Type': 'text/html'});
