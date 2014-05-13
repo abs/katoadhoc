6f9a1162e3c7273bfb5d1ecfb76d12c8eb4f349a
Jasmin Gacic <jasmin.gacic@gmail.com>
2014-05-13 10:56:02 +0200

    Added Kato Gem

--- ror/JWT.rb
+++ ror/JWT.rb
@@ -3,23 +3,8 @@ class KatoRoomsController < ApplicationController
   # GET /kato_rooms
   # GET /kato_rooms.json
   def index
-
-    pkey = '__pkey__'
-    skey = '__skey__'
-    duration = __duration__
     
-    token = JWT.encode(
-    {"exp"=>(((Time.now.to_f * 1000).to_i)+duration.to_i),
-      "user_id"=>__userid__,
-      "user_name"=>"__username__",
-      "room_id"=>__roomid__,
-      "room_name"=>"__roomname__",
-      "user_email" => "",
-      "welcome_text" => "Welcome",
-      "welcome_robot_name" => "Welcoming robot"},skey)
-
-    url = 'https://kato.im/adhoc#/' + pkey + '/' + token
-
+    url = KatoAdHocExpress.generateKey('__pkey__','__skey__', __duration__,'__userid__', '__username__','__roomid__','__roomname__')
     respond_to do |format|
       format.html {render action: "index"}
       format.json {render json: { "JWTToken" => url}}
