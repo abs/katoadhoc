c0a93fdf44e355c78007a4df01c40e447c651db6
Elvis <elvis@maestralsolutions.com>
2014-05-21 11:39:23 +0200

    node js added cookie

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,4 +1,3 @@
-var jwt = require('jwt-simple');
 var express = require('express');
 var kato = require('kato-adhoc-express');
 var fs = require('fs');
@@ -7,45 +6,43 @@ var app = express();
 var index = fs.readFileSync('index.html');
 
 var katoSettings = {
-	    // Required
-    pKey: '__pkey__',
-
     // Required
-    sKey: '__skey__',
+    sKey: 'EMxxK0z32ULskJgTKlgrouB6C9fDIjkXq92UPb1ICwk',
 
     // Required, expiration epoch in seconds
-    exp: __duration__,
+    exp: 3600,
 
     // Required, unique user id
-    user_id : __userid__,
+    user_id : '1',
 
     // Required, unique room id
-    user_name : '__username__',
+    user_name : 'Andrei',
 
     // Required, unique room id
-    room_id : __roomid__,
+    room_id : '302',
 
-    // Required, room name                
-    room_name : '__roomname__',
+    // Required, room name
+    room_name : 'Room 302',
 
-	//**********NOTE*********** 
+	//**********NOTE***********
 	//If these values are not used please remov
     // Optional, user email address (visible in tooltip over user name in history) must be unique when used within the scope of a given public key
-    user_email: '',
+    user_email: 'soroker@gmail.com',
 
     // Optional, text message to be sent out by welcome robot when room is created
     welcome_text: '',
 
     // Optional, name of welcome robot, if not specified defaults to 'Welcome Robot'
-    welcome_robot_name: '', 
+    welcome_robot_name: '',
 };
 
-var token = kato.generateUrl(katoSettings);
+// var token = kato.generateUrl(katoSettings);
 
 app.get('/', function(req, res){
-   res.writeHead(200, {'Content-Type': 'text/html'});
-   res.write(index.replace('__token__',token));
-   res.end();
+    kato.setInfo(katoSettings, res);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(index);
+    res.end();
 });
 
 var server = app.listen(4848, function() {
