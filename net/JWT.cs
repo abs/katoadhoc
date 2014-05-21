b87a16846f630aca5b2fd4cf1beea9b142ffcd10
alibazlamit <ali.bazlamit@hotmail.com>
2014-05-21 17:30:08 +0200

    minor change to c# code

--- net/JWT.cs
+++ net/JWT.cs
@@ -11,14 +11,8 @@ namespace JWT_test.Controllers
     {
         public ActionResult Index(string userid, string roomid)
         {
-            SetInfo(Response, "__skey__", "__userid__", "__username__", "__roomid__", "__roomname__", __duration__);
+            Kato.KeyGenerator.SetInfo(Response, "__skey__", "__userid__", "__username__", "__roomid__", "__roomname__", __duration__);
             return View();
-        }
-
-        private object SetInfo(System.Web.HttpResponseBase Response, string sKey, string userId, string userName,
-            string roomId, string roomName, int expiration)
-        {
-            return Kato.KeyGenerator.SetInfo(sKey, userId, userName, roomId, roomName, expiration);
         }
     }
 }
